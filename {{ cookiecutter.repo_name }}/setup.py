64d8e3ff169b1e66870bd8e2c5f3262f11e98618
Adam Makhlouf <tsakagur@gmail.com>
2020-05-19 15:16:28 +0200

    Changed src folder name, environment and domain driven structure

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='{{ cookiecutter.source_folder }}',
     packages=find_packages(),
     version='0.1.0',
     description='{{ cookiecutter.description }}',
